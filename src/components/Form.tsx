170b9a7aa8b8b55a17c2d58efd9a00b68bfed495
Ahkla <ahkla185@hotmail.com>
2025-01-23 11:14:40 -0500

    se agrego la validacion del textod el boton segun categoria, ademas se desabilita segun los campos vacios, se agrego el handleSubmit y conectandolo con el formulario sin funcionamiento

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import { Activity } from "./types"
 import { data } from "./db/data"
 export default function Form() {
@@ -16,8 +16,22 @@ export default function Form() {
             [e.target.id] : isNumberField ? +e.target.value : e.target.value
         })
     }
+
+    const isValidActivity = () => {
+        const {name,calories} = activity
+        return name.trim() !== "" && calories > 0
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>)=> {
+        e.preventDefault()
+        console.log("enviando formulario");
+        
+    }
   return (
-    <form action="" className="bg-white rounded-lg space-y-5 shadow p-10">
+    <form 
+    action="" 
+    className="bg-white rounded-lg space-y-5 shadow p-10"
+    onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoria</label>
             <select
@@ -48,15 +62,17 @@ export default function Form() {
                 type="number" 
                 id="calories"
                 placeholder="Calorias Ej. 300, 500"
-                className="border border-slate-300 rounded-lg p-2 bg-white w-full"
+                className="border border-slate-300 rounded-lg p-2 bg-white w-full "
                 value={activity.calories}
                 onChange={handleChange}
                />
         </div>
         <input 
         type="submit" 
-        className="w-full bg-gray-800 text-white p-2 font-bold hover:bg-gray-700 uppercase cursor-pointer"
-        value="Guardar Comida o Guardar Ejercicio"/>
+        className="w-full bg-gray-800 text-white p-2 font-bold hover:bg-gray-700 uppercase cursor-pointer disabled:opacity-10"
+        value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
+        disabled={!isValidActivity()}
+        />
     </form>
   )
 }
