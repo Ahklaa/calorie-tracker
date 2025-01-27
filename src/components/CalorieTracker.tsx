bd2122978dd3a4cea594fbaf05242e272fb067ac
Ahkla <ahkla185@hotmail.com>
2025-01-27 12:32:27 -0500

    se completo el proyecto

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -7,6 +7,7 @@ type CalorieTrackerProps = {
 export default function CalorieTracker({activities} : CalorieTrackerProps) {
     const caloriesConsumed = useMemo(()=>activities.reduce((total,activity) => activity.category === 1 ? total + activity.calories : total, 0 ),[activities])
     const caloriesBurned = useMemo(()=>activities.reduce((total,activity) => activity.category === 2 ? total + activity.calories : total, 0 ),[activities])
+    const netCalories = useMemo(()=>caloriesConsumed - caloriesBurned ,[activities])
   return (
     <>
         <h2 className="text-center text-4xl font-black text-white">Resumen de Calorias</h2>   
@@ -18,6 +19,10 @@ export default function CalorieTracker({activities} : CalorieTrackerProps) {
             <CaloriesDisplay 
                 calories={caloriesBurned}
                 text= "Ejercicio"
+            />
+            <CaloriesDisplay 
+                calories={netCalories}
+                text= "Diferencia"
             />
         </div>
     </>
